d8d632141f5182065707a54a019a1eb943ea6413
sajjad-mugdho <sajjadmugdho@yahoo.com>
2024-11-06 02:30:55 +0600

    [Done] added booking room client

--- src/app/book-room/page.tsx
+++ src/app/book-room/page.tsx
@@ -1,9 +1,14 @@
+import BookRoomClient from "@/components/Booking/BookRoomClient";
 import React from "react";
 
 type Props = {};
 
 const BookRoom = (props: Props) => {
-  return <div>BookRoom</div>;
+  return (
+    <div>
+      <BookRoomClient />
+    </div>
+  );
 };
 
 export default BookRoom;
---
+++ src/components/Booking/BookRoomClient.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const BookRoomClient = (props: Props) => {
+  return <div>BookRoomClient</div>;
+};
+
+export default BookRoomClient;
